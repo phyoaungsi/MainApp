9194b67d7767c5b7929ec4e399546e724526bd5c
phyo <phyo@kapps.com.sg>
2016-09-01 18:36:44 +0800

    1st initial version

---
+++ BuyNow/app/src/androidTest/java/shoopingcart/mm/com/pas/buynow/ApplicationTest.java
@@ -0,0 +1,13 @@
+package shoopingcart.mm.com.pas.buynow;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
