72e721b490c0db77c511fed7646040156ef1e360
phyo <phyo@kapps.com.sg>
2016-12-22 16:41:52 +0800

    Removed menu from detail activity

--- ShoopingCart/app/src/main/java/pas/com/mm/shoopingcart/DetailActivity.java
+++ ShoopingCart/app/src/main/java/pas/com/mm/shoopingcart/DetailActivity.java
@@ -153,14 +153,14 @@ public class DetailActivity extends AppCompatActivity implements DetailFragment.
         super.onDestroy();
         Log.i("DetailActivity","Destory");
     }
-
+/**
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
+**/
 
 
     @Override
