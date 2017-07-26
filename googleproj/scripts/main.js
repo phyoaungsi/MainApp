bfa45d76e99a1c6cd07c47bda33d4f2c4456db2e
phyo <phyo@kapps.com.sg>
2017-07-26 18:52:55 +0800

    update

--- googleproj/scripts/main.js
+++ googleproj/scripts/main.js
@@ -17,6 +17,7 @@
 
 // Shortcuts to DOM Elements.
 var messageForm = document.getElementById('message-form');
+var updateForm = document.getElementById('update-form');
 var messageInput = document.getElementById('new-post-message');
 var titleInput = document.getElementById('new-post-title');
 var code = document.getElementById('new-post-code');
@@ -40,6 +41,17 @@ var myTopPostsMenuButton = document.getElementById('menu-my-top-posts');
 var listeningFirebaseRefs = [];
 var storageRef = firebase.storage().ref();
 var keyHidden=document.getElementById('updateKey');
+var updateButton=document.getElementById('saveSubmit');
+
+ var messageInputU = document.getElementById('edit-post-message');
+			var titleInputU = document.getElementById('edit-post-title');
+			var codeU = document.getElementById('edit-post-code');
+			var typeU = document.getElementById('edit-post-cat');
+			//var desc = document.getElementById('edit-post-desc');
+			var amountInputU = document.getElementById('edit-post-amount');
+			var imageUrlU = document.getElementById('edit-post-imageUrl');
+			var editTextU = document.getElementById('edit-post-message');
+var format='<html><head><style type=\"text/css\">@font-face{    font-family: Zawgyi-One;    src: url("fonts/zawgyi.ttf") }    body{   font-family: TharLon;  background-color:#f3e5f5;   color:#9c27b0;    text-align: justify;}</style></head><body>${body}</body></html>'
 /**
  * Saves a new post to the Firebase DB.
  */
@@ -73,13 +85,17 @@ function writeNewPost(uid, username, picture, title, body,desc,amount,code,type,
 			  
 			  var date=new Date();
 			  var date_=JSON.stringify(date.toString());
+			  var htmlStr=getHtml(body);
+			  var discountAmount=parseFloat($('#discount-price').val());
 			  var postData = {
 				author: username,
 				uid: uid,
-				body: body,
+				htmlDetail: htmlStr,
+				textDetail: body,
 				title: title,
 				description:title,
 				amount:amount,
+				discount:discountAmount,
 				code:code,
 				type:type,
 				imgUrl:url,
@@ -92,7 +108,7 @@ function writeNewPost(uid, username, picture, title, body,desc,amount,code,type,
 
 			  // Write the new post's data simultaneously in the posts list and the user's post list.
 			  var updates = {};
-			  updates['/posts/' + newPostKey] = postData;
+			  updates['/message/items/' + newPostKey] = postData;
 			  updates['/user-posts/' + uid + '/' + newPostKey] = postData;
 
 			  firebase.database().ref().update(updates);
@@ -146,7 +162,7 @@ function toggleStar(postRef, uid) {
 /**
  * Creates a post element.
  */
-function createPostElement(postId, title, text, author, authorId, authorPic,imgUrl) {
+function createPostElement(postId, title, text, author, authorId, authorPic,imgUrl,code) {
   var uid = firebase.auth().currentUser.uid;
 
   var html =
@@ -161,22 +177,13 @@ function createPostElement(postId, title, text, author, authorId, authorPic,imgU
               '<div class="avatar"></div>' +
               '<div class="username mdl-color-text--black"></div>' +
             '</div>' +
-			'<button type="button"   class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Edit</button>'+
+		
           '</div>' +
-          '<span class="star">' +
-            '<div class="not-starred material-icons">star_border</div>' +
-            '<div class="starred material-icons">star</div>' +
-            '<div class="star-count">0</div>' +
-          '</span>' +
+         
           '<div class="text"></div>' +
-		  '<div class="imageContainer"><img height="200px" id="img1" src="'+imgUrl+'" /></div>' +
+			'<div class="mdl-card__actions mdl-card--border"><button type="button"   class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Edit</button></div>'+
           '<div class="comments-container"></div>' +
-          '<form class="add-comment" action="#">' +
-            '<div class="mdl-textfield mdl-js-textfield">' +
-              '<input class="mdl-textfield__input new-comment" type="text">' +
-              '<label class="mdl-textfield__label">Comment...</label>' +
-            '</div>' +
-          '</form>' +
+         
         '</div>' +
       '</div>';
 
@@ -184,102 +191,62 @@ function createPostElement(postId, title, text, author, authorId, authorPic,imgU
   var div = document.createElement('div');
   div.innerHTML = html;
   var postElement = div.firstChild;
+  /**
   if (componentHandler) {
     componentHandler.upgradeElements(postElement.getElementsByClassName('mdl-textfield')[0]);
   }
+  **/
 var editButton = postElement.getElementsByClassName('mdl-button')[0];
-  var addCommentForm = postElement.getElementsByClassName('add-comment')[0];
-  var commentInput = postElement.getElementsByClassName('new-comment')[0];
-  var star = postElement.getElementsByClassName('starred')[0];
-  var unStar = postElement.getElementsByClassName('not-starred')[0];
+
+ // var star = postElement.getElementsByClassName('starred')[0];
+ // var unStar = postElement.getElementsByClassName('not-starred')[0];
 
   // Set values.
   postElement.getElementsByClassName('text')[0].innerText = text;
-  postElement.getElementsByClassName('mdl-card__title-text')[0].innerText = title;
+  postElement.getElementsByClassName('mdl-card__title-text')[0].innerText = '('+ code +')'+title;
   postElement.getElementsByClassName('username')[0].innerText = author || 'Anonymous';
   postElement.getElementsByClassName('avatar')[0].style.backgroundImage = 'url("' +
       (authorPic || './silhouette.jpg') + '")';
 
-  // Listen for comments.
-  // [START child_event_listener_recycler]
-  var commentsRef = firebase.database().ref('post-comments/' + postId);
-  commentsRef.on('child_added', function(data) {
-    addCommentElement(postElement, data.key, data.val().text, data.val().author);
-  });
 
-  commentsRef.on('child_changed', function(data) {
-    setCommentValues(postElement, data.key, data.val().text, data.val().author);
-  });
 
-  commentsRef.on('child_removed', function(data) {
-    deleteComment(postElement, data.key);
-  });
-  // [END child_event_listener_recycler]
 
-  // Listen for likes counts.
-  // [START post_value_event_listener]
-  var starCountRef = firebase.database().ref('posts/' + postId + '/starCount');
-  starCountRef.on('value', function(snapshot) {
-    updateStarCount(postElement, snapshot.val());
-  });
-  // [END post_value_event_listener]
-
-  // Listen for the starred status.
-  var starredStatusRef = firebase.database().ref('posts/' + postId + '/stars/' + uid)
-  starredStatusRef.on('value', function(snapshot) {
-    updateStarredByCurrentUser(postElement, snapshot.val());
-  });
-
-  // Keep track of all Firebase reference on which we are listening.
-  listeningFirebaseRefs.push(commentsRef);
-  listeningFirebaseRefs.push(starCountRef);
-  listeningFirebaseRefs.push(starredStatusRef);
-
-  // Create new comment.
-  addCommentForm.onsubmit = function(e) {
-    e.preventDefault();
-    createNewComment(postId, firebase.auth().currentUser.displayName, uid, commentInput.value);
-    commentInput.value = '';
-    commentInput.parentElement.MaterialTextfield.boundUpdateClassesHandler();
-  };
-
-  // Bind starring action.
-  var onStarClicked = function() {
-    var globalPostRef = firebase.database().ref('/posts/' + postId);
-    var userPostRef = firebase.database().ref('/user-posts/' + authorId + '/' + postId);
-    toggleStar(globalPostRef, uid);
-    toggleStar(userPostRef, uid);
-  };
-  unStar.onclick = onStarClicked;
-  star.onclick = onStarClicked;
-
-  var onStarClicked = function() {
-    var globalPostRef = firebase.database().ref('/posts/' + postId);
-    var userPostRef = firebase.database().ref('/user-posts/' + authorId + '/' + postId);
-    toggleStar(globalPostRef, uid);
-    toggleStar(userPostRef, uid);
-  };
   editButton.onclick = function() {
     showSection(updatePost);
 	
-	 var getSinglePostRef = firebase.database().ref('/posts/'+postId);
+	 var getSinglePostRef = firebase.database().ref('/message/items/'+postId);
 	
 	 getSinglePostRef.once('value').then(function(data) {
-				  var messageInput = document.getElementById('edit-post-message');
-			var titleInput = document.getElementById('edit-post-title');
-			var code = document.getElementById('edit-post-code');
-			var type = document.getElementById('edit-post-cat');
-			var desc = document.getElementById('edit-post-desc');
-			var amount = document.getElementById('edit-post-amount');
-			var imageUrl = document.getElementById('edit-post-imageUrl');
+				 
 			
-				 titleInput.value  = data.val().title;
-				code.value = data.val().code;
-				type.value  = data.val().type;
-				desc.value  = data.val().description;
-				amount.value  = data.val().amount;
-				imageUrl.value  = data.val().imgUrl;
+				 titleInputU.value  = data.val().title;
+				codeU.value = data.val().code;
+				//type.value  = data.val().type;
+				var type_=data.val().type;
+				var amount0=data.val().amount;
+				//desc.value  = data.val().description;
+				amountInputU.value  = data.val().amount+"";
+				imageUrlU.value  = data.val().imgUrl;
+				editTextU.value=data.val().textDetail;
 				keyHidden.value=data.key;
+				$('#discount-price-edit').val(data.val().discount);
+				if(type_=="promo"){
+				document.getElementById('option2edit').parentNode.MaterialRadio.check();
+					}
+				else if(type_=="new"){
+					document.getElementById('option3edit').parentNode.MaterialRadio.check();
+					}
+				else if(type_=="regular"){
+
+					document.getElementById('option1edit').parentNode.MaterialRadio.check();
+					
+					}
+				else if(type_=="unlisted"){
+
+					document.getElementById('option4edit').parentNode.MaterialRadio.check();
+					
+					}
+				$("#editImg").attr('src', data.val().imgUrl );
           
 		  });
 		  document.getElementById('add').style.display='none';
@@ -289,15 +256,7 @@ var editButton = postElement.getElementsByClassName('mdl-button')[0];
 }
 
 /**
- * Writes a new comment for the given post.
- */
-function createNewComment(postId, username, uid, text) {
-  firebase.database().ref('post-comments/' + postId).push({
-    text: text,
-    author: username,
-    uid: uid
-  });
-}
+
 
 /**
  * Updates the starred status of the post.
@@ -319,36 +278,9 @@ function updateStarCount(postElement, nbStart) {
   postElement.getElementsByClassName('star-count')[0].innerText = nbStart;
 }
 
-/**
- * Creates a comment element and adds it to the given postElement.
- */
-function addCommentElement(postElement, id, text, author) {
-  var comment = document.createElement('div');
-  comment.classList.add('comment-' + id);
-  comment.innerHTML = '<span class="username"></span><span class="comment"></span>';
-  comment.getElementsByClassName('comment')[0].innerText = text;
-  comment.getElementsByClassName('username')[0].innerText = author || 'Anonymous';
 
-  var commentsContainer = postElement.getElementsByClassName('comments-container')[0];
-  commentsContainer.appendChild(comment);
-}
 
-/**
- * Sets the comment's values in the given postElement.
- */
-function setCommentValues(postElement, id, text, author) {
-  var comment = postElement.getElementsByClassName('comment-' + id)[0];
-  comment.getElementsByClassName('comment')[0].innerText = text;
-  comment.getElementsByClassName('fp-username')[0].innerText = author;
-}
 
-/**
- * Deletes the comment of the given ID in the given postElement.
- */
-function deleteComment(postElement, id) {
-  var comment = postElement.getElementsByClassName('comment-' + id)[0];
-  comment.parentElement.removeChild(comment);
-}
 
 /**
  * Starts listening for new posts and populates posts lists.
@@ -359,7 +291,7 @@ function startDatabaseQueries() {
   var topUserPostsRef = firebase.database().ref('user-posts/' + myUserId).orderByChild('starCount');
   // [END my_top_posts_query]
   // [START recent_posts_query]
-  var recentPostsRef = firebase.database().ref('posts').limitToLast(100);
+  var recentPostsRef = firebase.database().ref('message/items/').limitToLast(100);
   // [END recent_posts_query]
   var userPostsRef = firebase.database().ref('user-posts/' + myUserId);
 
@@ -368,7 +300,7 @@ function startDatabaseQueries() {
       var author = data.val().author || 'Anonymous';
       var containerElement = sectionElement.getElementsByClassName('posts-container')[0];
       containerElement.insertBefore(
-          createPostElement(data.key, data.val().title, data.val().body, author, data.val().uid, data.val().authorPic,data.val().imgUrl),
+          createPostElement(data.key, data.val().title, data.val().textDetail, author, data.val().uid, data.val().authorPic,data.val().imgUrl,data.val().code),
           containerElement.firstChild);
     });
     postsRef.on('child_changed', function(data) {	
@@ -376,7 +308,7 @@ function startDatabaseQueries() {
 		var postElement = containerElement.getElementsByClassName('post-' + data.key)[0];
 		postElement.getElementsByClassName('mdl-card__title-text')[0].innerText = data.val().title;
 		postElement.getElementsByClassName('username')[0].innerText = data.val().author;
-		postElement.getElementsByClassName('text')[0].innerText = data.val().body;
+		postElement.getElementsByClassName('text')[0].innerText = data.val().textDetail;
 		postElement.getElementsByClassName('star-count')[0].innerText = data.val().starCount;
     });
     postsRef.on('child_removed', function(data) {
@@ -482,6 +414,7 @@ function showSection(sectionElement, buttonElement) {
   userPostsSection.style.display = 'none';
   topUserPostsSection.style.display = 'none';
   addPost.style.display = 'none';
+   updatePost.style.display = 'none';
   recentMenuButton.classList.remove('is-active');
   myPostsMenuButton.classList.remove('is-active');
   myTopPostsMenuButton.classList.remove('is-active');
@@ -502,17 +435,70 @@ function getSequenceNumber()
 	var getConfigRef = firebase.database().ref('/config/code');
 	var updateRef = firebase.database().ref('/config/');
 	 getConfigRef.once('value').then(function(data) {
-				  $("#new-post-code").val("TEST"+data.val());
+				  $("#new-post-code").val("TWP"+data.val());
 					 
 						var temp=Number(data.val())+1;
 						updateRef.update({"code": temp});
 		  });
 }
 
+
+function update()
+{
+	
+	var titleu= titleInputU.value;
+	var typeu=$('input[name=rtypeedit]:checked').val();
+	var amountU=parseFloat(amountInputU.value);
+	var textU=editTextU.value;
+	var discountU=parseFloat($('#discount-price-edit').val());
+	var result=checkDiscount(discountU,typeu);
+	if(result==false)
+	{return;}
+	var key_=keyHidden.value;
+			
+	  var htmlText=getHtml(textU);
+	 firebase.database().ref('message/items/' + key_).update({
+						
+				htmlDetail: htmlText,
+				textDetail: textU,
+				title: titleu,
+				amount:amountU,
+				type:typeu,
+				discount:discountU
+						
+			  });
+			  alert("Updated Successfully!");
+			   recentMenuButton.click();
+	
+}
+
+
+function checkDiscount(discount,type_)
+{
+	if(type_=="promo" && (discount=="" || discount==0)){
+		
+		alert("Enter promotion amount");
+		return false;
+	}
+	return true;
+	
+}
+
+function getHtml(detail)
+{
+	var temp=detail.replace(/[\n\r]/g, "<br/>"); 
+	return format.replace("${body}", temp); 
+	
+}
 // Bindings on load.
 window.addEventListener('load', function() {
 
-
+ updateForm.onsubmit = function(e) {
+	 
+	 e.preventDefault();
+	 update();
+ document.getElementById('add').style.display='block';
+ };
    
 	
   // Bind Sign in button.
@@ -535,19 +521,17 @@ window.addEventListener('load', function() {
     var text = messageInput.value;
     var title = titleInput.value;
 	var code_=code.value;
-	var desc_=desc.value;
+	var desc_=titleInput.value;
 	var type_=$('input[name=rtype]:checked').val();
-	var amount=Number($("#discount-price").val());
-	if(type_=="promo" && (amount==0)){
-		
-		alert("Enter promotion amount");
-		return;
-	}
-	var amount_=amount.value;
+	var discount=Number($("#discount-price").val());
+	var result=checkDiscount(discount,type_);
+	if(result==false)
+	{return;}
+	var amount_=parseFloat(amount.value);
 	var imgUrl=imageUrl.value;
     if (text && title) {
       newPostForCurrentUser(title, text,desc_,amount_,code_,type_,imgUrl).then(function() {
-        myPostsMenuButton.click();
+        recentMenuButton.click();
       });
       messageInput.value = '';
       titleInput.value = '';
@@ -556,13 +540,15 @@ window.addEventListener('load', function() {
 	
 	}
   };
+  
+
 
   deleteButton.onclick= function(){
   
-  var r = confirm("Are you sure?");
+  var r = confirm("Are you sure to Delete?");
     if (r == true) {
         var id=keyHidden.value;
-		var getSinglePostRef = firebase.database().ref('/posts/');
+		var getSinglePostRef = firebase.database().ref('/message/items/');
 		getSinglePostRef.child(id).remove();
 		
 		
@@ -577,7 +563,8 @@ window.addEventListener('load', function() {
 // Delete the file
 			desertRef.delete().then(function() {
   // File deleted successfully
-          alert('Deleted');
+          alert('Deleted.');
+		    recentMenuButton.onclick();
 		}).catch(function(error) {
 		  // Uh-oh, an error occurred!
 		});
@@ -588,7 +575,7 @@ window.addEventListener('load', function() {
         var n = str.indexOf("images%2F");
 		var m = str.indexOf("?");
 		var len="images%2F".length;
-		alert(str.substring(n+len,m));
+	
        var txt = "You pressed Cancel!";
     }
     
@@ -596,6 +583,7 @@ window.addEventListener('load', function() {
   // Bind menu buttons.
   recentMenuButton.onclick = function() {
     showSection(recentPostsSection, recentMenuButton);
+	 document.getElementById('add').style.display='block';
   };
   myPostsMenuButton.onclick = function() {
     showSection(userPostsSection, myPostsMenuButton);
